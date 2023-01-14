e71b9ee0d878a9e355c38376fd2ae8b0e0918149
jh2ee <121475533+jh2ee@users.noreply.github.com>
2023-01-14 19:15:49 +0900

    [Silver IV] Title: 카드2, Time: 4 ms, Memory: 4156 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.cc
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    int n; cin>>n;
+    queue<int> q;
+    for(int i=1;i<=n;i++) q.push(i);
+    
+    while(q.size()>1){
+        q.pop();
+        int num=q.front();
+        q.push(num); q.pop();
+    }
+    cout<<q.front();
+}
