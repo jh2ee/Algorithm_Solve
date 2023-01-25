c943b570a7609efd873272600ee69399b3e468fb
jh2ee <121475533+jh2ee@users.noreply.github.com>
2023-01-25 17:20:38 +0900

    [Platinum V] Title: 오아시스 재결합, Time: 76 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Platinum/3015. 오아시스 재결합/오아시스 재결합.cc
+++ 백준/Platinum/3015. 오아시스 재결합/오아시스 재결합.cc
@@ -1,37 +1,38 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int n;
-    long long ans=0;
-    stack<pair<int,int> > s; // h, 같은 h원소 수의 쌍
-    cin>>n;
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    stack<pair<int,int> > s;
+    long long cnt=0;
+    int n; cin>>n;
+
     while(n--){
         int h; cin>>h;
 
-        //큰 수
         while(!s.empty()&&s.top().first<h){
-            ans+=s.top().second; //같은 원소 끼리의 쌍 + h와의 쌍
+            cnt+=s.top().second;
             s.pop();
         }
 
-        if(s.empty()) s.push(make_pair(h,1));
-        else{
-            //같은 수
-            if(s.top().first==h){ 
-                int cnt=s.top().second;
-                s.pop();
-                if(!s.empty()) ans++;
-
-                ans+=cnt;
-                s.push(make_pair(h,1+cnt)); //1+cnt=같은 원소끼리의 쌍 + 이후에 올 값과의 쌍
+        if(!s.empty()){
+            if(s.top().first>h){
+                cnt++;
             }
-            //작은 수
-            else{
-                ans++; //이전 top과의 쌍
-                s.push(make_pair(h,1)); //이후에 올 값과의 쌍
+            if(s.top().first==h){
+                cnt+=s.top().second;
+                s.top().second++;
+                if (s.size() != 1) cnt++;
+                continue;
             }
         }
+        s.push({h,1});
     }
-    cout<<ans;
+    cout<<cnt;
+
+    return 0;
 }
