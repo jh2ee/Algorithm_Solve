ede698dd1a8b5194ecbb9edabcb6a90f7b5a81ee
jh2ee <121475533+jh2ee@users.noreply.github.com>
2023-01-28 03:23:46 +0900

    [Silver III] Title: N과 M (6), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.cc
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n,m;
+int num[9]; //숫자 입력받음
+int arr[9]; //수열 저장
+bool used[9];
+
+void func(int k){
+    if(k==m){
+        for(int i=0;i<m;i++){
+            cout<<arr[i]<<" ";
+        }
+        cout<<"\n";
+        return;
+    }
+    for(int i=0;i<n;i++){
+        if(used[i]) continue;
+        if(k!=0&&arr[k-1]>num[i]) continue;
+        arr[k]=num[i];
+        used[i]=1;
+        func(k+1);
+        used[i]=0;
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    cin>>n>>m;
+    for(int i=0;i<n;i++){
+        cin>>num[i];
+    }
+    sort(num,num+n);
+    func(0);
+
+    return 0;
+}
