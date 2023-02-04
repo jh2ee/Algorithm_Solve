06e10d91fa197a5601b545975aa3481832178e3d
jh2ee <121475533+jh2ee@users.noreply.github.com>
2023-02-05 01:06:46 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int d[15];
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    d[1]=1; d[2]=2; d[3]=4;
+    for(int i=4;i<=11;i++){
+        d[i]=d[i-1]+d[i-2]+d[i-3];
+    }
+    int t; cin>>t;
+    while(t--){
+        int n; cin>>n;
+        cout<<d[n]<<"\n";
+    }
+    
+    return 0;
+}
