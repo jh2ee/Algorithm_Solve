cab3a66da68f66000883bbbcfc8911af96c8fc37
jh2ee <121475533+jh2ee@users.noreply.github.com>
2024-08-15 18:44:34 +0900

    [level 2] Title: 타겟 넘버, Time: 390.26 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,19 +1,19 @@
-answer = 0
 
+answer = 0
 def solution(numbers, target):    
     
     n = len(numbers)
     
-    def dfs(index, result):
-        if index == n:
-            if result == target:
-                global answer 
+    def dfs(index, result): # 현재값, 결과값?
+        if index == n: # numbers 모두 탐색 경우
+            if result == target: # 탐색 결과 target과 같은 경우 answer+=1
+                global answer
                 answer += 1
             return
-        else:
+        else: # 탐색하는 경우
             dfs(index + 1, result + numbers[index])
             dfs(index + 1, result - numbers[index])
-    
-    dfs(0, 0)
+
+    dfs(0, 0) # numbers[0]부터 탐색
     
     return answer
